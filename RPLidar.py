0a2027d22823d791e375b05a1550b2df91909761
Justin Hadyniak <68020879+Jhadyniak1@users.noreply.github.com>
2026-02-22 17:13:41 -0500

    Update RPLidar initialization and live plotting (using robotica library)
    Refactor RPLidar setup and plotting functionality.

--- RPLidar.py
+++ RPLidar.py
@@ -1,56 +1,40 @@
-## install dependencies via terminal on raspi
-#pip install adafruit-circuitpython-rplidar
-### https://learn.adafruit.com/slamtec-rplidar-on-pi/cpython-on-raspberry-pi
-import os
-from math import cos, sin, pi, floor
-from adafruit_rplidar import RPLidar
-import time 
+from rplidar import RPLidar
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0'
-lidar = RPLidar(None, PORT_NAME, BAUDRATE = 115200)
-time.sleep(2)
-# used to scale data to fit on the screen
-max_distance = 0
-scan_data = [0]*360
+
+lidar = RPLidar(PORT_NAME, baudrate=115200)
+
+# Set up polar plot
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.set_facecolor('#0a0c10')
+fig.patch.set_facecolor('#0a0c10')
+ax.set_title('RPLidar Live Scan', color='cyan')
+ax.tick_params(colors='gray')
+scatter = ax.scatter([], [], s=2, c='cyan', alpha=0.8)
+
+plt.ion()
+plt.show()
 
 try:
-    print(lidar.info)
     for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            scan_data[min([359, floor(angle)])] = distance
-        process_data(scan_data)
+        angles    = np.radians([p[1] for p in scan])
+        distances = [p[2] for p in scan]
+
+        scatter.set_offsets(np.c_[angles, distances])
+        scatter.set_array(np.array(distances))  # color by distance
+        
+        ax.set_ylim(0, max(distances) if distances else 1000)
+        
+        fig.canvas.flush_events()
+        plt.pause(0.01)
 
 except KeyboardInterrupt:
-    print('Stoping.')
+    print("Stopping...")
+
+finally:
     lidar.stop()
+    lidar.stop_motor()
     lidar.disconnect()
-
-
-def process_data(data):
-    global max_distance
-    for angle in range(360):
-        distance = data[angle]
-        if distance > 0:                  # ignore initially ungathered data points
-            max_distance = max([min([5000, distance]), max_distance])
-            radians = angle * pi / 180.0
-            x = distance * cos(radians)
-            y = distance * sin(radians)
-            point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance * 119))
-            
-            r = distance
-            theta = radians
-            
-            fig, axs = plt.subplots(2, 1, figsize=(5, 8), subplot_kw={'projection': 'polar'},
-                                    layout='constrained')
-            ax = axs[0]
-            ax.plot(theta, r)
-            ax.set_rmax(10)
-            ax.set_rticks([0.5, 1, 1.5, 2])  # Fewer radial ticks
-            ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
-            ax.grid(True)
-            ax.set_title("Lidar live data read", va='bottom')
-            plt.show()
+    plt.close()

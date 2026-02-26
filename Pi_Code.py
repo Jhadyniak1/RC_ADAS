b000da30cc5a314de8d0f27eb22eaad18841e2d5
Justin Hadyniak <68020879+Jhadyniak1@users.noreply.github.com>
2026-02-25 20:01:03 -0500

    Simplify IR signal handling in main function
    Removed unnecessary waiting and command retrieval logic.

--- Pi_Code.py
+++ Pi_Code.py
@@ -42,27 +42,8 @@ def get_next_event(dev):
 
 def main():
     device = get_ir_device()
-
-    print("Waiting 5 seconds for IR signals.  A list of all received commands will be returned.")
-    sleep(5)
-    events = get_all_events(device)
-    try:
-        event_list = [event.value for event in events]
-        print("Received commands:", event_list, "\n")
-    except BlockingIOError:
-        print("No commands received.\n")
-
-    print("Waiting 5 seconds for IR signals.  The last received command will be returned.")
-    sleep(5)
-    last_event = get_last_event(device)
-    if last_event is not None:
-        print("Received command:", last_event.value, "\n")
-    else:
-        print("No commands received.\n")
-
-    print("Waiting indefinitely for IR signals.  The first received command will be returned.")
-    next_event = get_next_event(device)
-    print("Received command:", next_event.value, "\n")
+    mode = get_last_event(device)
+    print("Received command:", mode.value, "\n")
 
 if __name__ == "__main__":
     main()

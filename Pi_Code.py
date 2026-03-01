b015fa7786efa06fd4106ae8e41c5175d8f3a8c5
Justin Hadyniak <68020879+Jhadyniak1@users.noreply.github.com>
2026-03-01 18:23:41 -0500

    Refactor command handling in main loop

--- Pi_Code.py
+++ Pi_Code.py
@@ -3,6 +3,11 @@
 #sudo ir-keytable -p all (to enable all protocols)
 #sudo ir-keytable -t (to test)
 #!/usr/bin/python
+#source env/bin/activate
+#https://ignorantofthings.com/receiving-infrared-on-the-raspberry-pi-with-python/
+#sudo ir-keytable -p all (to enable all protocols)
+#sudo ir-keytable -t (to test)
+#!/usr/bin/python
 import evdev
 from time import sleep
 import serial
@@ -30,7 +35,7 @@ def get_last_event(dev):
             if (event.value > 0):
                 last_event = event
     except BlockingIOError: # no events to be read
-        last_event = 0
+        last_event = None
 
     return last_event
 
@@ -62,45 +67,48 @@ STAR = 0x16, 22
 POUND = 0x0d, 13
 '''
 def main():
-    ser = serial.Serial('/dev/ttyS0', 9600, timeout=1) #'/dev/ttyACM0 if using the actual USB port'
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #'/dev/ttyACM0 if using the actual USB port, /dev/ttyS0 for wires'
     sleep(2)
     device = get_ir_device()
     while True:
         mode = get_last_event(device)
         if mode is not None:
             print("Received command:", mode.value, "\n")
+        
+            if mode.value == 69: # Lane keep!
+                message = f"{5},{255}\n"
+                print("Lane keep mode")
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 70: # Object avoidance 
+                message = f"{5},{125}\n"
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 71: # Object detection 
+                message = f"{6},{255}\n"
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 68: # Adaptive cruise
+                message = f"{10},{255}\n"
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 24: # Up key
+                message = f"{11},{255}\n"
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 82: # Down key
+               message = f"{11},{255}\n"
+               ser.write(message.encode('utf-8'))
+            elif mode.value == 8: # Left key
+                message = f"{11},{255}\n"
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 90: # Right key
+                message = f"{11},{255}\n"
+                ser.write(message.encode('utf-8'))
+            elif mode.value == 13: #Pound sign
+                print("Shutting down")
+                sys.exit()
+            else:  #Unknown command
+                print("Unrecognized command")
+        
+        
         else:
             print("No commands received.\n")
-        
-        if mode.value == 69: # Lane keep!
-            message = f"{5},{255}\n"
-            print("Lane keep mode")
-            ser.write(message.encode('utf-8'))
-        if mode.value == 70: # Object avoidance 
-            message = f"{5},{125}\n"
-            ser.write(message.encode('utf-8'))
-        if mode == 71: # Object detection 
-            message = f"{6},{255}\n"
-            ser.write(message.encode('utf-8'))
-        if mode == 68: # Adaptive cruise
-            message = f"{10},{255}\n"
-            ser.write(message.encode('utf-8'))
-        if mode == 24: # Up key
-            message = f"{11},{255}\n"
-            ser.write(message.encode('utf-8'))
-        if mode == 82: # Down key
-           message = f"{11},{255}\n"
-           ser.write(message.encode('utf-8'))
-        if mode == 8: # Left key
-            message = f"{11},{255}\n"
-            ser.write(message.encode('utf-8'))
-        if mode == 90: # Right key
-            message = f"{11},{255}\n"
-            ser.write(message.encode('utf-8'))
-        if mode == 13: #Pound sign
-            print("Shutting down")
-            sys.exit()
         sleep(0.5)
-            
 if __name__ == "__main__":
     main()

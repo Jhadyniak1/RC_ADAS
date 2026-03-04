223aaf1dbcf6948c0ba7c3c85e81f009daec9365
Justin Hadyniak <68020879+Jhadyniak1@users.noreply.github.com>
2026-03-03 21:06:14 -0500

    Integrate OLED display for mode and control feedback
    Added OLED display functionality to show mode, throttle, and steering values.

--- Pi_Code.py
+++ Pi_Code.py
@@ -11,6 +11,40 @@
 import evdev
 from time import sleep
 import serial
+
+import board
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+
+import adafruit_ssd1306
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
+i2c = board.I2C()  # uses board.SCL and board.SDA
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+# Clear display.
+oled.fill(0)
+oled.show()
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
+# Load default font.
+font = ImageFont.load_default(size = 14) ### https://pillow.readthedocs.io/en/stable/reference/ImageFont.html
+#font1 = ImageFont.load("arial.pil")
+text = "ECE 4415 Group 4"
+draw.text(
+    (0, 0),
+    text,
+    font=font,
+    fill=255,
+)
+
 # returns path of gpio ir receiver device
 def get_ir_device():
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
@@ -66,6 +100,8 @@ OK = 0x1c, 28
 STAR = 0x16, 22
 POUND = 0x0d, 13
 '''
+throttle = 0
+steering = 0
 def main():
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #'/dev/ttyACM0 if using the actual USB port, /dev/ttyS0 for wires'
     sleep(2)
@@ -76,37 +112,81 @@ def main():
             print("Received command:", mode.value, "\n")
         
             if mode.value == 69: # Lane keep!
-                message = f"{5},{255}\n"
+                throttle = 5 #need to be changed by the algorithm
+                steering = 255 #need to be changed by the algorithm
+                controls = f"{throttle},{steering}\n"
                 print("Lane keep mode")
-                ser.write(message.encode('utf-8'))
+                ser.write(controls.encode('utf-8'))
+                mode_message = "Lane keep"
+                
             elif mode.value == 70: # Object avoidance 
-                message = f"{5},{125}\n"
-                ser.write(message.encode('utf-8'))
+                throttle = 5 #need to be changed by the algorithm
+                steering = 125 #need to be changed by the algorithm
+                controls = f"{throttle},{steering}\n"
+                ser.write(controls.encode('utf-8'))
+                mode_message = "Object Avoidance"
+                
             elif mode.value == 71: # Object detection 
-                message = f"{6},{255}\n"
-                ser.write(message.encode('utf-8'))
+                throttle = 6 #need to be changed by the algorithm
+                steering = 255 #need to be changed by the algorithm
+                controls = f"{throttle},{steering}\n"
+                mode_message = "Object Detection"
+                
             elif mode.value == 68: # Adaptive cruise
-                message = f"{10},{255}\n"
-                ser.write(message.encode('utf-8'))
+                throttle = 10 #need to be changed by the algorithm
+                steering = 255 #need to be changed by the algorithm
+                controls = f"{throttle},{steering}\n"
+                ser.write(controls.encode('utf-8'))
+                mode_message = "Adaptive cruise"
+                
             elif mode.value == 24: # Up key
-                message = f"{11},{255}\n"
-                ser.write(message.encode('utf-8'))
+                throttle = 255 #need to be changed by the algorithm
+                steering = 0 #need to be changed by the algorithm
+                controls = f"{throttle},{steering}\n"
+                mode_message = "Lane keep"
+                
             elif mode.value == 82: # Down key
-               message = f"{11},{255}\n"
-               ser.write(message.encode('utf-8'))
+               controls = f"{11},{255}\n"
+               ser.write(controls.encode('utf-8'))
+               mode_message = "Lane keep"
+                
             elif mode.value == 8: # Left key
-                message = f"{11},{255}\n"
-                ser.write(message.encode('utf-8'))
+                controls = f"{11},{255}\n"
+                ser.write(controls.encode('utf-8'))
+                mode_message = "Lane keep"
+            
             elif mode.value == 90: # Right key
-                message = f"{11},{255}\n"
-                ser.write(message.encode('utf-8'))
+                controls = f"{11},{255}\n"
+                ser.write(controls.encode('utf-8'))
+                
             elif mode.value == 13: #Pound sign
                 print("Shutting down")
                 sys.exit()
+                
             else:  #Unknown command
                 print("Unrecognized command")
         
-        
+        text1 = "Mode:" + mode_message
+        draw.text(
+            (0, 15),
+            text1,
+            font=font,
+            fill=255)
+        text2 = "Throttle: " + throttle
+        draw.text(
+            (0, 35),
+            text2,
+            font=font,
+            fill=255)
+        text3 = "Steering: " + steering
+        draw.text(
+            (0, 45),
+            text3,
+            font=font,
+            fill=255)
+        # Display image
+        oled.image(image)
+        oled.show()
         else:
             print("No commands received.\n")
         sleep(0.5)

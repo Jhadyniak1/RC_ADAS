a233eca18755f224f9b29623c6ee4d128aefba3e
Justin Hadyniak <68020879+Jhadyniak1@users.noreply.github.com>
2026-03-10 13:28:23 -0400

    Create object_detection_test.py for OpenCV usage
    Add object detection script with OpenCV setup instructions

---
+++ supporting_files/object_detection_test.py
@@ -0,0 +1,94 @@
+'''
+sudo apt-get update && sudo apt-get upgrade
+sudo nano /etc/dphys-swapfile
+The change the number on CONF_SWAPSIZE = 100 to CONF_SWAPSIZE=2048.
+
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo pip3 install numpy
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.4.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.4.0.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+cd ~/opencv-4.4.0/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+
+                                -D CMAKE_INSTALL_PREFIX=/usr/local \
+
+                                -D INSTALL_PYTHON_EXAMPLES=ON \
+
+                                -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-4.4.0/modules \
+
+                                -D BUILD_EXAMPLES=ON ..
+
+make -j $(nproc) # also is the command to retry
+sudo make install && sudo ldconfig
+sudo reboot
+At this point the majority of the installation process is complete and you can now change back the Swapfile so that the CONF_SWAPSIZE = 100. 
+
+
+'''
+#Import the Open-CV extra functionalities
+import cv2
+
+#This is to pull the information about what each object is called
+classNames = []
+classFile = "/home/pi/Desktop/Object_Detection_Files/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+#This is to pull the information about what each object should look like
+configPath = "/home/pi/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/pi/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
+
+#This is some set up values to get good results
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+#This is to set up what the drawn box size/colour is and the font/size/colour of the name tag and confidence label   
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+#Below has been commented out, if you want to print each sighting of an object to the console you can uncomment below     
+#print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in objects: 
+                objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0] 10,box[1] 30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0] 200,box[1] 30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+    
+    return img,objectInfo
+
+#Below determines the size of the live feed window that will be displayed on the Raspberry Pi OS
+if __name__ == "__main__":
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+    #cap.set(10,70)
+    
+#Below is the never ending loop that determines what will happen when an object is identified.    
+    while True:
+        success, img = cap.read()
+#Below provides a huge amount of controll. the 0.45 number is the threshold number, the 0.2 number is the nms number)
+        result, objectInfo = getObjects(img,0.45,0.2)
+        #print(objectInfo)
+        cv2.imshow("Output",img)
+        cv2.waitKey(1)
+
